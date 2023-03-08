f12c39e9f494d2aebb476757a15995de2d526694
Lorenz Kuhn <lorenz.kuhn@cs.ox.ac.uk>
2023-03-08 15:04:16 +0000

    Add parsing files

---
+++ code/parse_coqa.py
@@ -0,0 +1,97 @@
+import json
+
+import evaluate
+import pandas as pd
+import torch
+from datasets import Dataset
+from transformers import AutoModelForSequenceClassification, AutoTokenizer
+
+import config
+
+with open(f'{config.data_dir}/coqa-dev-v1.0.json', 'r') as infile:
+  data = json.load(infile)['data']
+
+
+rouge = evaluate.load('rouge')
+
+tokenizer = AutoTokenizer.from_pretrained("microsoft/deberta-large-mnli")
+
+model = AutoModelForSequenceClassification.from_pretrained("microsoft/deberta-large-mnli").cuda()
+
+dataset = {}
+
+dataset['story'] = []
+dataset['question'] = []
+dataset['answer'] = []
+dataset['additional_answers'] = []
+dataset['rouge1'] = []
+dataset['rouge2'] = []
+dataset['rougeL'] = []
+dataset['semantic_variability'] = []
+dataset['id'] = []
+
+
+for sample_id, sample in enumerate(data):
+    story = sample['story']
+    questions = sample['questions']
+    answers = sample['answers']
+    additional_answers = sample['additional_answers']
+    for question_index, question in enumerate(questions):
+        dataset['story'].append(story)
+        dataset['question'].append(question['input_text'])
+        dataset['answer'].append({'text': answers[question_index]['input_text'], 'answer_start':answers[question_index]['span_start'] })
+        dataset['id'].append(sample['id'] + '_' + str(question_index))
+        additional_answers_list = []
+
+        for i in range(3):
+            additional_answers_list.append(additional_answers[str(i)][question_index]['input_text'])
+        
+        dataset['additional_answers'].append(additional_answers_list)
+        story = story + ' Q: ' + question['input_text'] + ' A: ' + answers[question_index]['input_text']
+        if not story[-1] == '.':
+            story = story + '.'
+        all_answers = [answers[question_index]['input_text']] + additional_answers_list
+
+        answer_list_1 = []
+        answer_list_2 = []
+        has_semantically_different_answers = False
+        inputs = []
+        
+        # This computes the syntactic similarity across the reference answers
+        for i, reference_answer in enumerate(all_answers):
+            for j in range(4):
+                if i != j:
+                    answer_list_1.append(all_answers[i])
+                    answer_list_2.append(all_answers[j])
+
+                    qa_1 = question['input_text'] + ' ' + all_answers[i]
+                    qa_2 = question['input_text'] + ' ' + all_answers[j]
+
+                    input = qa_1 + ' [SEP] ' + qa_2
+                     
+                    inputs.append(input)
+                    #print(encoded_input)
+        
+        encoded_input = tokenizer.batch_encode_plus(inputs, padding=True)              
+        
+        prediction = model(torch.tensor(encoded_input['input_ids'], device='cuda'))['logits']
+        
+        predicted_label = torch.argmax(prediction, dim=1)
+        if 0 in predicted_label:
+            has_semantically_different_answers = True
+
+        dataset['semantic_variability'].append(has_semantically_different_answers)   
+
+
+        results = rouge.compute(predictions=answer_list_1, references=answer_list_2)
+        dataset['rouge1'].append(results['rouge1'].mid.fmeasure)
+        dataset['rouge2'].append(results['rouge2'].mid.fmeasure)
+        dataset['rougeL'].append(results['rougeL'].mid.fmeasure)
+            
+
+dataset_df = pd.DataFrame.from_dict(dataset)
+
+dataset = Dataset.from_pandas(dataset_df)
+
+dataset.save_to_disk(f'{config.data_dir}/coqa_dataset')
+
---
+++ code/parse_triviaqa.py
@@ -0,0 +1,85 @@
+
+import argparse
+import pathlib
+import pickle
+
+import accelerate
+import datasets
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+import config
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--type_of_question', type=str)
+parser.add_argument('--num_generations_per_prompt', type=int, default=5)
+parser.add_argument('--fraction_of_data_to_use', type=float, default=0.9)
+parser.add_argument('--model', type=str, default='opt-350m')
+parser.add_argument('--run_id', type=str, default='run_1')
+parser.add_argument('--temperature', type=float, default='1.0')
+parser.add_argument('--num_beams', type=int, default='5')
+parser.add_argument('--decoding_method', type=str, default='beam_search')
+parser.add_argument('--top_p', type=float, default=1.0)
+args = parser.parse_args()
+
+model = AutoModelForCausalLM.from_pretrained(f"facebook/{args.model}", torch_dtype=torch.float16, cache_dir=config.data_dir).cuda()
+tokenizer = AutoTokenizer.from_pretrained(f"facebook/opt-350m", use_fast=False, cache_dir=config.data_dir)
+
+if args.model == 'opt-30b':
+    accelerate.dispatch_model(model, device_map=config.device_map)
+
+seed_value= 10 
+
+if not pathlib.Path(f'{config.data_dir}/trivia_qa').exists():
+
+    print('Preprocessing dataset')
+    val_data = datasets.load_dataset("trivia_qa", "rc.nocontext", split="validation")
+    train_data = datasets.load_dataset("trivia_qa", "rc.nocontext", split="train")
+    data_for_few_shot_prompt = train_data.select(range(0, 10))
+
+    few_shot_prompt = 'This is a bot that correctly answers questions. \n'
+    for sample in data_for_few_shot_prompt:
+        few_shot_prompt += 'Question: ' + sample['question'] + ' Answer: ' + sample['answer']['value'] + ' '
+
+    batch_size=4  # change to 16 for full training
+    encoder_max_length=1024
+    decoder_max_length=128
+
+    def process_data_to_model_inputs(batch):
+        # tokenize the inputs and labels
+        answers = [answer["value"] for answer in batch["answer"]]
+        
+        batch_with_prompt = [few_shot_prompt + "Question: " + question + " Answer:" for question in batch["question"]]
+        inputs = tokenizer(batch_with_prompt, padding=False, truncation=False)
+        outputs = tokenizer(answers, padding=False, truncation=False) 
+
+        batch["input_ids"] = inputs.input_ids
+        batch["attention_mask"] = inputs.attention_mask
+        batch["decoder_input_ids"] = outputs.input_ids
+        batch["decoder_attention_mask"] = outputs.attention_mask
+        batch["labels"] = outputs.input_ids.copy()
+        batch['answer'] = answers
+
+        # because BERT automatically shifts the labels, the labels correspond exactly to `decoder_input_ids`. 
+        # We have to make sure that the PAD token is ignored
+        batch["labels"] = [[-100 if token == tokenizer.pad_token_id else token for token in labels] for labels in batch["labels"]]
+
+        return batch
+
+    val_data = val_data.map(
+        process_data_to_model_inputs, 
+        batched=True, 
+        batch_size=batch_size, 
+        remove_columns=["search_results", "question_source", "entity_pages"]
+    )
+    val_data.set_format(
+        type="torch", columns=["input_ids", "attention_mask", "decoder_input_ids", "decoder_attention_mask", "labels"], output_all_columns=True
+    )
+
+    val_data.save_to_disk(f'{config.data_dir}/trivia_qa')
+else:
+    
+    val_data = datasets.load_from_disk(f'{config.data_dir}/trivia_qa')
+
+
+
