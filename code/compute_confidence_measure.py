27adbf0dc1bf056c771c205d89c2a79cbd82dc3a
Lorenz Kuhn <lorenz.kuhn@cs.ox.ac.uk>
2023-04-27 17:37:44 +0100

    Update compute_confidence_measure.py

--- code/compute_confidence_measure.py
+++ code/compute_confidence_measure.py
@@ -123,7 +123,6 @@ def get_predictive_entropy(log_likelihoods):
 def get_predictive_entropy_over_concepts(log_likelihoods, semantic_set_ids):
     """Compute the semantic entropy"""
     mean_across_models = torch.logsumexp(log_likelihoods, dim=0) - torch.log(torch.tensor(log_likelihoods.shape[0]))
-    mean_across_models -= torch.max(torch.nan_to_num(torch.logsumexp(mean_across_models, dim=1), nan=-100))
     # This is ok because all the models have the same semantic set ids
     semantic_set_ids = semantic_set_ids[0]
     entropies = []
